3cf38ae0731a31ef493863f243e7f42d07d4e1c2
Nathan Sanford <nwsanfor24@gmail.com>
2020-01-29 14:37:40 -0500

    add times to table

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const m = moment();
 currentDay.innerText = m.format('LL');
 
 // Create a new table row element
+const button = document.getElementById("button");
 var tRow = $("<tr>");
 
-displayResult.addEventListener("click", function(){
-    $('.table > tbody:last-child').append('<tr>Hello World</tr><tr>Hello Universe</tr>');
+button.addEventListener("click", function() {
+    $('#table > tbody:last-child').append('<tr>Hello World</tr><tr>Hello Universe</tr>');
 });
