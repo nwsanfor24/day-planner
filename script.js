ab73009b5d63fdab7cff5dfbc7bdc8a2ab0fef99
Nathan Sanford <nwsanfor24@gmail.com>
2020-01-31 11:05:57 -0500

    added a function to save user input to Local Storage

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ currentDay.innerText = m.format('LL');
 
 const button = document.getElementById("button");
 
+let input = [];
 
-button.addEventListener("click", function(event) {
-    alert(event.target);
+const addUserInput = (ev)=>{
+    ev.preventDefault(); // stops the form submitting
+    let input = {
+        id: Date.now(),
+        input: document.getElementById('userInput').value,
+    }
+    input.push(input);
+    document.forms[0].reset(); // to clear the form for the next entry
+
+    //for display purposes only
+    console.warn('added', {input} );
+
+    //saving to Local Storage
+    localStorage.setItem('UserInput', JSON.stringify(input) );
+    
+}
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById("btn").addEventListener('click', addUserInput);
 });
