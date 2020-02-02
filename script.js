b589ecb2c095842a0e1a2f95e24d5bc874fd6800
Nathan Sanford <nwsanfor24@gmail.com>
2020-02-02 15:53:15 -0500

    btn saves items to local storage

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
-//Display current date at top of the page
-const currentDay = document.getElementById("currentDay");
-const m = moment();
+$(document).ready(function () {
+    //Display current date at top of the page
+    const currentDay = document.getElementById("currentDay");
+    const m = moment();
 
 
-currentDay.innerText = m.format('LL');
+    currentDay.innerText = m.format('LL');
 
-// When save button is clicked, it saves the text in the input field to the local storage
+    // When save button is clicked, it saves the text in the input field to the local storage
 
-const button = document.getElementById("button");
-
-let userInput = [];
-
-const addUserInput = (event)=>{
-    event.preventDefault(); // stops the form submitting
-    let userInput = {
-        id: Date.now(),
-        userInput: document.getElementById('userInput').value,
-    }
-    userInput.push(userInput);
-    document.forms[0].reset(); // to clear the form for the next entry
-
-    //for display purposes only
-    console.warn('added', {userInput} );
-
-    //saving to Local Storage
-    localStorage.setItem('UserInput', JSON.stringify(userInput) );
-    
-}
-addUserInput();
+    const button = document.getElementById("button");
 
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById("btn").addEventListener('click', addUserInput);
-});
+    $("#btn").on("click", function (event) {
+        event.preventDefault();
+
+
+        let userInput = $("#userInput").val().trim();
+        console.log(userInput)
+
+
+        localStorage.setItem("UserInput", JSON.stringify(userInput));
+
+        JSON.parse(localStorage.getItem(userInput));
+    });
+
+    function clear() {
+        $("#article-section").empty();
+    };
+
+    $("#clear-all").on("click", clear);
+
+    // const addUserInput = (event)=>{
+    //     event.preventDefault(); // stops the form submitting
+    //     let userInput = {
+    //         id: Date.now(),
+    //         userInput: document.getElementById('userInput').value,
+    //     }
+    //     userInput.push(userInput);
+    //     document.forms[0].reset(); // to clear the form for the next entry
+
+    //     //for display purposes only
+    //     console.warn('added', {userInput} );
+
+    //     //saving to Local Storage
+    //     localStorage.setItem('UserInput', JSON.stringify(userInput) );
+
+    // }
+    // addUserInput();
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById("btn").addEventListener('click', addUserInput);
+    });
+})
